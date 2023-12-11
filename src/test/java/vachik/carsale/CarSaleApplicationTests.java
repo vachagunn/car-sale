3cf7ae80336d8f5cd8f6b207012af039e6934ef9
vachagunn <vachikpuper@gmail.com>
2023-12-11 12:37:53 +0500

    Create Spring project

---
+++ src/test/java/vachik/carsale/CarSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package vachik.carsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
