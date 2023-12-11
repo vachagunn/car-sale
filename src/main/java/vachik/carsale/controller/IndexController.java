9060f445c10e1884e873bb5919d03e4ddb27a2a8
vachagunn <vachikpuper@gmail.com>
2023-12-11 12:45:20 +0500

    test model creation

--- src/main/java/vachik/carsale/controller/IndexController.java
+++ src/main/java/vachik/carsale/controller/IndexController.java
@@ -3,10 +3,17 @@ package vachik.carsale.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+import java.util.Objects;
+
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
+    public String index(Map<String, Object> model) {
+        model.put("key1", 15);
+        model.put("key2", 200);
+        model.put("key3", "Строка");
+
         return "index";
     }
 }
