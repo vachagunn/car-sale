33b2204c50dfd0af7d5d46a424decdb8b09eb7d7
vachagunn <vachikpuper@gmail.com>
2023-12-21 18:49:10 +0500

    Displaying car cards on the "catalog" page.

--- src/main/java/vachik/carsale/controller/CatalogController.java
+++ src/main/java/vachik/carsale/controller/CatalogController.java
@@ -2,10 +2,17 @@ package vachik.carsale.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vachik.carsale.entity.CarCard;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @Slf4j
 @Controller
@@ -13,6 +20,26 @@ import java.util.Map;
 public class CatalogController {
     @GetMapping("/")
     public String index(Map<String, Object> model) {
+        Random random = new Random();
+        int count = random.nextInt(2, 6);
+
+        List<CarCard> cards = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            cards.add(
+                    new CarCard(i + 1, "Mercedes-Benz", "GL", "black", 2014, 220000, 3200000)
+            );
+        }
+
+        System.out.println(cards);
+        model.put("cards", cards);
+
+        return "catalog";
+    }
+
+    @PostMapping("/cards")
+    public String cards(@RequestBody MultiValueMap<String, String> formData) {
+        log.info("Пришли карточки {}", formData);
         return "catalog";
     }
 }
---
+++ src/main/java/vachik/carsale/entity/CarCard.java
@@ -0,0 +1,3 @@
+package vachik.carsale.entity;
+
+public record CarCard(int id, String brand, String model, String color, int year, int mileage, int price) {}
