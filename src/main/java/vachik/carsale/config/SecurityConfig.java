384130287da8dd5f0c528463f54edcce671beb6d
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-15 18:44:41 +0500

    Added SecurityConfig

---
+++ src/main/java/vachik/carsale/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package vachik.carsale.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider autoProvider = new DaoAuthenticationProvider();
+
+        autoProvider.setUserDetailsService(userDetailsService);
+        autoProvider.setPasswordEncoder(passwordEncoder());
+
+        return autoProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(form -> form.failureForwardUrl("/error"))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/login", "/").permitAll().anyRequest().authenticated()
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+        return http.build();
+    }
+}
