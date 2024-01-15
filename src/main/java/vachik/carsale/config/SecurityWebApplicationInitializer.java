5f8a59342070ce51ca0cc715ffc403bf5a07210a
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-15 19:36:20 +0500

    Added Security Initializer

---
+++ src/main/java/vachik/carsale/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package vachik.carsale.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    public SecurityWebApplicationInitializer() {
+        super(SecurityConfig.class);
+    }
+}
